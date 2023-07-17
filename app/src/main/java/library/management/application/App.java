a9f9f2f9f29acd22af5d01958995dedf2f0e9d7f
Mathitharan Kajaluxsan (mathikaj) <mathikaj@students.zhaw.ch>
2023-07-17 16:33:45 +0200

    added corrections

--- app/src/main/java/library/management/application/App.java
+++ app/src/main/java/library/management/application/App.java
@@ -23,42 +23,46 @@ public class App {
                 break;
             }
 
-            switch (selectedNumber){
-                case 1:
-                    System.out.println("Enter the Title of the Book");
-                    Book book = new Book(scanner.nextLine());
-                    library.addBook(book);
-                    System.out.println(book.getTitle() + " added to the Library");
-                    break;
-
-                case 2:
-                    System.out.println("Enter the Client's name");
-                    String clientName =scanner.nextLine();
-                    library.addClient(clientName);
-                    break;
-
-                case 3:
-                    System.out.println("Enter the name of the Book");
-                    String name = scanner.nextLine();
-                    System.out.println("Enter Client's name");
-                    String client1 = scanner.nextLine();
-                    library.borrowBook(name, client1);
-                    break;
-
-                case 4:
-                    System.out.println("Enter the name of the Book");
-                    String bookName =scanner.nextLine();
-                    System.out.println("Enter the Client's name");
-                    String clientName1 = scanner.nextLine();
-                    library.returnBook(bookName, clientName1);
-                    break;
-
-                default:
-                    System.out.println("Invalid Choose");
-                    break;
-            }
+            actions(library, scanner, selectedNumber);
         }
 
         scanner.close();
+    }
+
+    private static void actions(Library library, Scanner scanner, int selectedNumber) {
+        switch (selectedNumber) {
+            case 1:
+                System.out.println("Enter the Title of the Book");
+                Book book = new Book(scanner.nextLine());
+                library.addBook(book);
+                System.out.println(book.getTitle() + " added to the Library");
+                break;
+
+            case 2:
+                System.out.println("Enter the Client's name");
+                String clientName = scanner.nextLine();
+                library.addClient(clientName);
+                break;
+
+            case 3:
+                System.out.println("Enter the name of the Book");
+                String name = scanner.nextLine();
+                System.out.println("Enter Client's name");
+                String client1 = scanner.nextLine();
+                library.borrowBook(name, client1);
+                break;
+
+            case 4:
+                System.out.println("Enter the name of the Book");
+                String bookName = scanner.nextLine();
+                System.out.println("Enter the Client's name");
+                String clientName1 = scanner.nextLine();
+                library.returnBook(bookName, clientName1);
+                break;
+
+            default:
+                System.out.println("Invalid Choose");
+                break;
+        }
     }
 }
